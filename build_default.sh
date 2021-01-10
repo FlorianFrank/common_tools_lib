d046220d83b77c5d3925666d6047a25fe8c956dc
Florian Frank <Florian.Frank@uni-passau.de>
2021-01-10 20:20:49 +0100

    Platform Independent Library: Collection of snippets to solve common tasks  - Fix build script install dir

--- build_default.sh
+++ build_default.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p tmp
-mkdir -p bin
+mkdir -p ./tmp
+mkdir -p ./bin
 pushd tmp
-cmake .. -DALL=1 -DINSTALL_DIR=../bin
+cmake .. -DALL=1 -DINSTALL_DIR="$PWD/bin"
 make
 make install
 popd
