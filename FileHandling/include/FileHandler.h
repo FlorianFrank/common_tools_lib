f523ddc28eb6fbefe6bb8145522c07c9fd3985e7
Florian Frank <Florian.Frank@uni-passau.de>
2021-03-23 09:55:11 +0100

    Platform Independent Library: Collection of snippets to solve common tasks  - Add function to open, write and close a file in one function call  - Add doxygen documentation comments

--- FileHandling/include/FileHandler.h
+++ FileHandling/include/FileHandler.h
@@ -8,8 +8,12 @@
 #include "ErrorCodeDefines.h"
 #include "FileHandlingDefines.h"
 
-PIL_ERROR_CODE OpenFile(FileHandle *fileHandle, const char* fileName, FilePermissions permission, FileMode fileMode);
-PIL_ERROR_CODE CloseFile(FileHandle *fileHandle);
-PIL_ERROR_CODE WriteFile(FileHandle *fileHandle, uint8_t* buffer, uint32_t *bufferLen);
-PIL_ERROR_CODE ReadFile(FileHandle *fileHandle, uint8_t* buffer, uint32_t *bufferLen);
+PIL_ERROR_CODE PIL_OpenFile(PIL_FileHandle *fileHandle, const char* fileName, PIL_FilePermissions permission, PIL_FileMode fileMode);
+PIL_ERROR_CODE PIL_CloseFile(PIL_FileHandle *fileHandle);
+PIL_ERROR_CODE PIL_WriteFile(PIL_FileHandle *fileHandle, uint8_t* buffer, uint32_t *bufferLen);
+PIL_ERROR_CODE PIL_ReadFile(PIL_FileHandle *fileHandle, uint8_t* buffer, const uint32_t *bufferLen);
+
+PIL_ERROR_CODE PIL_WriteDataToFile(const char* fileName, uint8_t* buffer, uint32_t bufferLen, PIL_FileMode mode);
+
+
 #endif //PLATFORMINDEPENDENTLIB_FILEHANDLER_H
--- FileHandling/src/FileHandler.c
+++ FileHandling/src/FileHandler.c
@@ -1,11 +1,15 @@
-//
-// Created by florianfrank on 22.03.21.
-//
-
 #include <stdio.h>
 #include "../include/FileHandler.h"
 
-PIL_ERROR_CODE OpenFile(FileHandle *fileHandle, const char* fileName, FilePermissions permission, FileMode fileMode)
+/**
+ * @brief This function opens a file for reading and writing
+ * @param fileHandle fileHandle returned for further actions, like reading and writing.
+ * @param fileName path and name of the file to open.
+ * @param permission permission of the file. (Read/write/Read_Write)
+ * @param fileMode Mode when opening in write mode. (Append, create and truncate)
+ * @return PIL_NO_ERROR when no error occurs.
+ */
+PIL_ERROR_CODE PIL_OpenFile(PIL_FileHandle *fileHandle, const char* fileName, PIL_FilePermissions permission, PIL_FileMode fileMode)
 {
     if(!fileHandle)
         return PIL_INVALID_ARGUMENTS;
@@ -31,11 +35,17 @@ PIL_ERROR_CODE OpenFile(FileHandle *fileHandle, const char* fileName, FilePermis
     if(!fileHandle->fileHandle)
         return PIL_ERRNO;
 
+    fileHandle->errCode = 0;
     fileHandle->isOpen = TRUE;
     return PIL_NO_ERROR;
 }
 
-PIL_ERROR_CODE CloseFile(FileHandle *fileHandle)
+/**
+ * @brief This function closes an open file.
+ * @param fileHandle file handle corresponding to the file to close.
+ * @return PIL_NO_ERROR if no error occurs.
+ */
+PIL_ERROR_CODE PIL_CloseFile(PIL_FileHandle *fileHandle)
 {
     if(!fileHandle)
         return PIL_INVALID_ARGUMENTS;
@@ -48,7 +58,14 @@ PIL_ERROR_CODE CloseFile(FileHandle *fileHandle)
     return PIL_NO_ERROR;
 }
 
-PIL_ERROR_CODE WriteFile(FileHandle *fileHandle, uint8_t* buffer, uint32_t *bufferLen)
+/**
+ * @brief This function writes the data passed by the buffer argument to the file.
+ * @param fileHandle file handle returned by the PIL_OpenFile function.
+ * @param buffer buffer to write.
+ * @param bufferLen number of byte to write. Also returns the number actually written.
+ * @return PIL_NO_ERROR if no error occurs.
+ */
+PIL_ERROR_CODE PIL_WriteFile(PIL_FileHandle *fileHandle, uint8_t* buffer, uint32_t *bufferLen)
 {
     if(!fileHandle)
         return PIL_INVALID_ARGUMENTS;
@@ -56,17 +73,25 @@ PIL_ERROR_CODE WriteFile(FileHandle *fileHandle, uint8_t* buffer, uint32_t *buff
     if(!fileHandle->isOpen)
         return PIL_INTERFACE_CLOSED;
 
-    int ret = fwrite(buffer, 1, *bufferLen, fileHandle->fileHandle);
-    if(ret < 0)
-        return PIL_ERRNO;
+    size_t ret = fwrite(buffer, 1, *bufferLen, fileHandle->fileHandle);
     if(ret < *bufferLen)
+    {
+        *bufferLen = ret;
+        fileHandle->errCode = ferror(fileHandle->fileHandle);
         return PIL_ONLY_PARTIALLY_READ_WRITTEN;
-
+    }
 
     return PIL_NO_ERROR;
 }
 
-PIL_ERROR_CODE ReadFile(FileHandle *fileHandle, uint8_t* buffer, uint32_t *bufferLen)
+/**
+ * @brief This function reads data from a file.
+ * @param fileHandle file handle returned by the PIL_OpenFile function.
+ * @param buffer return buffer contains the read data.
+ * @param bufferLen maximum length of the data which can be written into the buffer.
+ * @return PIL_NO_ERROR if no error occurs.
+ */
+PIL_ERROR_CODE PIL_ReadFile(PIL_FileHandle *fileHandle, uint8_t* buffer, const uint32_t *bufferLen)
 {
     if(!fileHandle)
         return PIL_INVALID_ARGUMENTS;
@@ -74,11 +99,39 @@ PIL_ERROR_CODE ReadFile(FileHandle *fileHandle, uint8_t* buffer, uint32_t *buffe
     if(!fileHandle->isOpen)
         return PIL_INTERFACE_CLOSED;
 
-    int ret = fread(buffer, 1, *bufferLen, fileHandle->fileHandle);
-    if(ret < 0)
-        return PIL_ERRNO;
-    if(ret < *bufferLen)
+    int sizeToWrite = *bufferLen;
+    size_t ret = fread(buffer, 1, *bufferLen, fileHandle->fileHandle);
+    if(ret < sizeToWrite)
+    {
+        *buffer = ret;
+        fileHandle->errCode = ferror(fileHandle->fileHandle);
         return PIL_ONLY_PARTIALLY_READ_WRITTEN;
+    }
 
     return PIL_NO_ERROR;
+}
+
+/**
+ * @brief This function creates a file, writes the data passed in the buffer argument to a file and closes the file.
+ * @param fileName path and name of the file.
+ * @param buffer the buffer contains the data to write.
+ * @param bufferLen length of the buffer to write
+ * @param mode mode of the operation (create, truncate, append)
+ * @return PIL_NO_ERROR if no error occurs
+ */
+PIL_ERROR_CODE PIL_WriteDataToFile(const char* fileName, uint8_t* buffer, uint32_t bufferLen, PIL_FileMode mode)
+{
+    if(!fileName || !buffer)
+        return PIL_INVALID_ARGUMENTS;
+
+    PIL_FileHandle fileHandle;
+    PIL_ERROR_CODE ret = PIL_OpenFile(&fileHandle, fileName, READ_WRITE, mode);
+    if(ret != PIL_NO_ERROR)
+        return ret;
+
+    ret = PIL_WriteFile(&fileHandle, buffer, &bufferLen);
+    if (ret != PIL_NO_ERROR)
+        return ret;
+
+    return PIL_CloseFile(&fileHandle);
 }
