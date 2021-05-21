8e6a13d0d0e0ee9a6b978bab07e2c0d3b2a9d3af
Florian Frank <Florian.Frank@uni-passau.de>
2021-05-21 16:27:55 +0200

    Add socket uniting

--- UnitTesting/SocketUnitTest.cpp
+++ UnitTesting/SocketUnitTest.cpp
@@ -1,40 +1,66 @@
 //
 // Created by florianfrank on 30.12.20.
 //
-#if 0
 
 #include <gtest/gtest.h> // google test
+#include <string>
 
 extern "C"
 {
 #include "Socket.h"
 }
 
+bool acceptFlag = true;
+std::string loram = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut "
+                    "labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores "
+                    "et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.";
+
+char recvBuff[4096];
+
+void receiveHandler(uint8_t *buffer, uint32_t len)
+{
+    strcpy(recvBuff, (char*)buffer);
+}
+
+
+void CallbackAccept(PIL_SOCKET sck, char* ip)
+{
+    acceptFlag = true;
+    uint32_t length = loram.length();
+    PIL_SOCKET_Send(&sck, reinterpret_cast<uint8_t*>((char*)loram.c_str()), &length);
+}
+
+
 /**
  * This test checks if an error code is returned when passing an invalid parameter,
  * like a nullptr or a range of 0.
  */
-TEST(SocketTest, SimpleSocketTest)
+TEST(SocketTest_C, SimpleSocketTest)
 {
-    PIL_SOCKET socket;
-    PIL_ERROR_CODE retCode = PIL_SOCKET_Create(&socket, UDP, IPv4, "127.0.0.1", 8080);
-    retCode = PIL_SOCKET_Bind(&socket);
-    retCode = PIL_SOCKET_Listen(&socket, 10);
-    retCode = PIL_SOCKET_Accept(&socket, "127.0.0.1");
-    retCode = PIL_SOCKET_Connect(&socket,"127.0.0.1", 8080);
-    retCode = PIL_SOCKET_WaitTillDataAvail(&socket, 10);
-    PIL_SOCKET_ReceiveFrom(&socket, nullptr, nullptr, nullptr, nullptr);
-    PIL_SOCKET_Receive(&socket, nullptr, nullptr);
-    PIL_SOCKET_Send(&socket, nullptr, nullptr);
-    PIL_SOCKET_SendTo(&socket, nullptr, 80, nullptr, nullptr);
-    PIL_SOCKET_GetSenderIP(&socket);
-    PIL_SOCKET_IsOpen(&socket);
-     PIL_SOCKET_Close(&socket);
 
+    PIL_SOCKET srvSocket;
+    PIL_ERROR_CODE ret = PIL_SOCKET_Setup_ServerSocket(&srvSocket, 14000, CallbackAccept);
+    EXPECT_EQ(ret,PIL_NO_ERROR);
 
+    acceptFlag = false;
 
-//Nonce nonce;
-EXPECT_EQ(1,1);
+    PIL_SOCKET clientSocket;
+    ret = PIL_SOCKET_ConnectToServer(&clientSocket, "127.0.0.1", 15000, 14000, NULL);
+    EXPECT_EQ(ret,PIL_NO_ERROR);
+
+    PIL_SOCKET_RegisterCallbackFunction(&clientSocket,receiveHandler);
+
+    usleep(100000);
+    EXPECT_EQ(acceptFlag, true);
+
+    EXPECT_STREQ(loram.c_str(), recvBuff);
+
+    ret = PIL_SOCKET_Close(&clientSocket);
+    EXPECT_EQ(ret, PIL_NO_ERROR);
+
+    ret = PIL_SOCKET_Close(&srvSocket);
+    //Nonce nonce;
+    EXPECT_EQ(ret, PIL_NO_ERROR);
 }
 
 /**
@@ -45,7 +71,3 @@ int main(int argc, char **argv)
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-#endif // IF 0
-
-int main()
-{return 0;}
