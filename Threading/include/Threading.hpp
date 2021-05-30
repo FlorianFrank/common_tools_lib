4321c54790ecbd656a22fc335e4de920aca81a87
Florian Frank <Florian.Frank@uni-passau.de>
2021-05-30 11:42:30 +0200

    Add CPP threading functionality.

---
+++ Threading/include/Threading.hpp
@@ -0,0 +1,38 @@
+//
+// Created by florianfrank on 30.05.21.
+//
+
+#ifndef PLATFORMINDEPENDENTLIB_THREADING_HPP
+#define PLATFORMINDEPENDENTLIB_THREADING_HPP
+
+#include "ErrorHandler.hpp"
+
+struct ThreadHandle;
+
+namespace PIL
+{
+
+    class Threading : public ErrorHandler
+    {
+    public:
+        Threading(void *(*threadFunction)(void *), void *argument);
+
+        void Run();
+
+        void Detach();
+
+        void Join();
+
+        void Abort();
+
+    private:
+        ThreadHandle *m_ThreadHandle;
+        void *m_argument;
+        void *m_RetValue;
+        void *(*m_ThreadFunction)(void *function);
+
+    };
+}
+
+
+#endif //PLATFORMINDEPENDENTLIB_THREADING_HPP
---
+++ Threading/src/Threading.cpp
@@ -0,0 +1,41 @@
+//
+// Created by florianfrank on 30.05.21.
+//
+#ifdef CXX
+
+
+#include "Threading.hpp"
+
+extern "C" {
+#include "Threading.h"
+};
+
+PIL::Threading::Threading(void *(*threadFunction)(void *), void *argument) : ErrorHandler(),
+m_ThreadFunction(threadFunction), m_argument(argument)
+{
+    m_LastError = PIL_THREADING_CreateThread(m_ThreadHandle, m_ThreadFunction, argument);
+}
+
+void PIL::Threading::Run()
+{
+    m_LastError = PIL_THREADING_RunThread(m_ThreadHandle, FALSE); // TODO fix
+}
+
+void PIL::Threading::Join()
+{
+    m_LastError = PIL_THREADING_JoinThread(m_ThreadHandle, &m_RetValue);
+}
+
+void PIL::Threading::Abort()
+{
+    m_LastError = PIL_THREADING_AbortThread(m_ThreadHandle);
+}
+
+void PIL::Threading::Detach()
+{
+    m_LastError = PIL_THREADING_Detach(m_ThreadHandle);
+}
+
+
+
+#endif // CXX
