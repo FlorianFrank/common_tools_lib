59dfdfa7041c4f31c1484d76671aac2b587655b7
Florian Frank <Florian.Frank@uni-passau.de>
2022-07-04 16:29:04 +0200

    Add FileHandling.cpp and FileHandling.hpp to the project

---
+++ FileHandling/include/ctlib/FileHandler.hpp
@@ -0,0 +1,21 @@
+//
+// Created by florianfrank on 04.07.22.
+//
+
+#ifndef INSTRUMENT_CONTROL_LIB_FILEHANDLER_HPP
+#define INSTRUMENT_CONTROL_LIB_FILEHANDLER_HPP
+
+#include "ctlib/PIL_FILE"
+
+namespace PIL {
+    class FileHandler
+    {
+    public:
+        FileHandler(std::string fileName, PIL_PER)
+
+    };
+}
+
+
+
+#endif //INSTRUMENT_CONTROL_LIB_FILEHANDLER_HPP
---
+++ FileHandling/src/FileHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by florianfrank on 04.07.22.
+//
+
+#include "../include/ctlib/FileHandler.hpp"
