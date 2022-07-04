47c28348ff53181885f295733d45ab7756b2f9a2
Florian Frank <Florian.Frank@uni-passau.de>
2022-07-04 16:54:50 +0200

    Add FileHandling.cpp

--- FileHandling/include/ctlib/FileHandler.hpp
+++ FileHandling/include/ctlib/FileHandler.hpp
@@ -5,17 +5,31 @@
 #ifndef INSTRUMENT_CONTROL_LIB_FILEHANDLER_HPP
 #define INSTRUMENT_CONTROL_LIB_FILEHANDLER_HPP
 
-#include "ctlib/PIL_FILE"
+extern "C" {
+#include "FileHandler.h"
+};
+
+#include <string>
 
 namespace PIL {
     class FileHandler
     {
     public:
-        FileHandler(std::string fileName, PIL_PER)
+        FileHandler(std::string &fileName, PIL_FilePermissions permissions, PIL_FileMode mode);
+        ~FileHandler();
 
+        PIL_ERROR_CODE CloseFile();
+        PIL_ERROR_CODE WriteFile(std::string &buffer, bool flush);
+        PIL_ERROR_CODE ReadFile(std::string *buffer);
+        PIL_ERROR_CODE LISTFilesInDirectory(std::string &path, uint8_t filer, PIL_FileListElem *listOfFiles, bool recursive);
+
+
+    private:
+        std::string m_FileName;
+        PIL_FilePermissions m_Permissions;
+        PIL_FileMode m_Mode;
+        PIL_FileHandle m_Handle;
+        PIL_ERROR_CODE m_LastError;
     };
-}
-
-
-
+};
 #endif //INSTRUMENT_CONTROL_LIB_FILEHANDLER_HPP
--- FileHandling/src/FileHandler.cpp
+++ FileHandling/src/FileHandler.cpp
@@ -1,5 +1,77 @@
-//
-// Created by florianfrank on 04.07.22.
-//
+/**
+ * @brief This File contains a C++ implementation wrapping the Filehandler file.
+ * @author Florian Frank
+ * @copyright University of Passau
+ */
+#include "ctlib/FileHandler.hpp"
+#include "ctlib/ExceptionHandler.h"
 
-#include "../include/ctlib/FileHandler.hpp"
+PIL::FileHandler::FileHandler(std::string &fileName, PIL_FilePermissions permissions, PIL_FileMode mode):
+m_FileName(fileName), m_Permissions(permissions), m_Mode(mode)
+{
+    m_LastError = PIL_OpenFile(&m_Handle, fileName.c_str(), m_Permissions, m_Mode);
+}
+
+
+PIL::FileHandler::~FileHandler()
+{
+    CloseFile();
+}
+
+
+PIL_ERROR_CODE PIL::FileHandler::CloseFile()
+{
+    auto ret = PIL_CloseFile(&m_Handle);
+#ifdef PIL_EXCEPTION_HANDLING
+    if(ret != PIL_NO_ERROR)
+        throw ExceptionHandler(ret);
+#endif // PIL_EXCEPTION_HANDLING
+    return ret;
+}
+
+
+PIL_ERROR_CODE PIL::FileHandler::WriteFile(std::string &buffer, bool flush)
+{
+    uint32_t bufferLen = buffer.length();
+    auto ret = PIL_WriteFile(&m_Handle, reinterpret_cast<uint8_t *>(const_cast<char *>(buffer.c_str())), &bufferLen,
+                             (flush) ? TRUE : FALSE);
+#ifdef PIL_EXCEPTION_HANDLING
+    if(ret != PIL_NO_ERROR)
+        throw ExceptionHandler(ret);
+#endif // PIL_EXCEPTION_HANDLING
+    return ret;
+}
+
+
+PIL_ERROR_CODE PIL::FileHandler::ReadFile(std::string *buffer)
+{
+    uint32_t buffLen = 1024; // TODO
+    uint8_t cBuff[1024];
+    auto ret = PIL_ReadFile(&m_Handle, cBuff, &buffLen);
+#ifdef PIL_EXCEPTION_HANDLING
+    if(ret != PIL_NO_ERROR)
+        throw ExceptionHandler(ret);
+#endif // PIL_EXCEPTION_HANDLING
+    if(!buffer)
+    {
+#ifdef PIL_EXCEPTION_HANDLING
+        if(ret != PIL_NO_ERROR)
+        throw ExceptionHandler(PIL_INVALID_ARGUMENTS);
+#endif // PIL_EXCEPTION_HANDLING
+        return PIL_INVALID_ARGUMENTS;
+    }
+    buffer->append(reinterpret_cast<char*>(cBuff));
+    return ret;
+}
+
+
+/*static*/ PIL_ERROR_CODE
+PIL::FileHandler::LISTFilesInDirectory(std::string &path, uint8_t filer, PIL_FileListElem *listOfFiles, bool recursive)
+{
+    auto ret = PIL_ListFilesInDirectory(path.c_str(), filer, listOfFiles, (recursive) ? TRUE : FALSE);
+#ifdef PIL_EXCEPTION_HANDLING
+    if(ret != PIL_NO_ERROR)
+        throw ExceptionHandler(PIL_INVALID_ARGUMENTS);
+#endif // PIL_EXCEPTION_HANDLING
+    return ret;
+}
