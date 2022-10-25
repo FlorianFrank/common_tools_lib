c54a4f6b04762d98473ecfd6cfc057bff6ed6b87
Florian Frank <Florian.Frank@uni-passau.de>
2022-10-25 20:17:26 +0200

    Add comment to DataTypeDefines.h

--- Additional/ctlib/DataTypeDefines.h
+++ Additional/ctlib/DataTypeDefines.h
@@ -1,14 +1,16 @@
-//
-// Created by florianfrank on 29.12.20.
-//
-
-#ifndef PLATFORMINDEPENDENTLIB_DATATYPEDEFINES_H
-#define PLATFORMINDEPENDENTLIB_DATATYPEDEFINES_H
+/**
+ * @brief This file serves as a wrapper for platform and architecture independent representation of data types.
+ * @author Florian Frank
+ */
+#ifndef PLATFORM_INDEPENDENT_LIB_DATATYPE_DEFINES_H
+#define PLATFORM_INDEPENDENT_LIB_DATATYPE_DEFINES_H
 
 #include <stdint.h>
 
+/** @brief Boolean data type implementation on C. **/
 typedef uint8_t PIL_BOOL;
-#define TRUE 1
-#define FALSE 0
 
-#endif //PLATFORMINDEPENDENTLIB_DATATYPEDEFINES_H
+#define TRUE    1
+#define FALSE   0
+
+#endif //PLATFORM_INDEPENDENT_LIB_DATATYPE_DEFINES_H
