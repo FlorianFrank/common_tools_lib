bce11aa63bf6b0f926620f501b4986ef691f0bc7
Florian Frank <Florian.Frank@uni-passau.de>
2022-12-28 11:45:48 +0100

    Add file handling unit test to the repo

---
+++ UnitTesting/FileHandlingUnitTest.cpp
@@ -0,0 +1,84 @@
+#include <gtest/gtest.h> // google test
+#include <ctlib/FileHandler.hpp>
+
+TEST(FileHandlingTestCpp, FileExists){
+    std::string fileName = "UnitTestFile.txt";
+    PIL::FileHandler fileHandler(fileName, READ_WRITE, CREATE_TRUNCATE);
+    std::string sampleText = "This is a sample text";
+    auto ret = fileHandler.WriteFile(sampleText, true);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+    ret = fileHandler.WriteFile(sampleText, true);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+
+    std::string retText;
+    ret = fileHandler.ReadFile(&retText);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+    ASSERT_STREQ((sampleText + sampleText).c_str(), retText.c_str());
+}
+
+TEST(FileHandlingTestCpp, FileTruncate){
+    std::string fileName = "FileTruncate.txt";
+    PIL::FileHandler fileHandler(fileName, READ_WRITE, CREATE_TRUNCATE);
+    std::string sampleText = "This is a sample text";
+    auto ret = fileHandler.WriteFile(sampleText, true);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+    PIL::FileHandler fileHandler2(fileName, READ_WRITE, CREATE_TRUNCATE);
+    ret = fileHandler2.WriteFile(sampleText, true);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+
+
+    std::string retText;
+    ret = fileHandler.ReadFile(&retText);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+    ASSERT_STREQ(sampleText.c_str(), retText.c_str());
+}
+
+TEST(FileHandlingTestCpp, FileAppend){
+    std::string fileName = "FileTruncate.txt";
+    PIL::FileHandler fileHandler(fileName, READ_WRITE, CREATE_TRUNCATE);
+    std::string sampleText = "This is a sample text";
+    auto ret = fileHandler.WriteFile(sampleText, true);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+    PIL::FileHandler fileHandler2(fileName, READ_WRITE, CREATE_APPEND);
+    ret = fileHandler2.WriteFile(sampleText, true);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+
+    std::string retText;
+    ret = fileHandler.ReadFile(&retText);
+    ASSERT_EQ(PIL_NO_ERROR, ret);
+    ASSERT_STREQ((sampleText+sampleText).c_str(), retText.c_str());
+}
+
+TEST(FileHandlingTestCpp, ListFilesDirectoriesOnly){
+    std::string path = ".";
+    PIL_FileListElem listOfFiles;
+    auto ret = PIL::FileHandler::ListFilesInDirectory(path, DIRECTORIES, &listOfFiles, false);
+    ASSERT_EQ(ret, PIL_NO_ERROR);
+    auto currentElem = &listOfFiles;
+    ASSERT_FALSE(currentElem->previous);
+    ASSERT_TRUE(currentElem->next);
+    while(currentElem)
+    {
+        if(currentElem->handle)
+            ASSERT_EQ(currentElem->handle->type, DIRECTORIES);
+        currentElem = currentElem->next;
+    }
+}
+
+TEST(FileHandlingTestCpp, ListFilesFilesOnly){
+    std::string path = ".";
+    PIL_FileListElem listOfFiles;
+    auto ret = PIL::FileHandler::ListFilesInDirectory(path, REGULAR_FILES, &listOfFiles, false);
+    ASSERT_EQ(ret, PIL_NO_ERROR);
+    auto currentElem = &listOfFiles;
+    ASSERT_FALSE(currentElem->previous);
+    ASSERT_TRUE(currentElem->next);
+    while(currentElem)
+    {
+        if(currentElem->handle){
+            printf("%s\n", currentElem->handle->path);
+            ASSERT_EQ(currentElem->handle->type, REGULAR_FILES);
+        }
+        currentElem = currentElem->next;
+    }
+}
