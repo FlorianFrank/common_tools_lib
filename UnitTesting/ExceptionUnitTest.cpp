f75f2bda307cfb80e764e97b8d5f8f52f5aaf00f
Florian Frank <Florian.Frank@uni-passau.de>
2022-12-28 09:13:57 +0100

    Cleanup ExceptionUnitTest.cpp

--- UnitTesting/ExceptionUnitTest.cpp
+++ UnitTesting/ExceptionUnitTest.cpp
@@ -1,18 +1,15 @@
 #include <gtest/gtest.h> // google test
-#include <string>
+
 #include <ctlib/Exception.h>
 #include "ctlib/Logging.h"
 
 TEST(ExceptionTest, SimpleExceptionNoArgs){
-    // this tests _that_ the expected exception is thrown
     EXPECT_THROW({
-                     try
-                     {
+                     try {
                          throw PIL::Exception(PIL_INVALID_ARGUMENTS);
                      }
                      catch(PIL::Exception &e)
                      {
-                         // and this tests that it has the correct message
                          EXPECT_STREQ( "Exception: Invalid arguments", e.what() );
                          throw;
                      }
@@ -20,15 +17,11 @@ TEST(ExceptionTest, SimpleExceptionNoArgs){
 }
 
 TEST(ExceptionTest, SimpleExceptionAdditionalErrorMessage){
-    // this tests _that_ the expected exception is thrown
     EXPECT_THROW({
-                     try
-                     {
+                     try {
                          throw PIL::Exception(PIL_INTERFACE_CLOSED, "Additional Error Message");
                      }
-                     catch(PIL::Exception &e)
-                     {
-                         // and this tests that it has the correct message
+                     catch(PIL::Exception &e){
                          EXPECT_STREQ( "Exception: Socket is closed (Additional Error Message)", e.what() );
                          throw;
                      }
@@ -36,15 +29,11 @@ TEST(ExceptionTest, SimpleExceptionAdditionalErrorMessage){
 }
 
 TEST(ExceptionTest, ExceptionFileNameLineNumberAdditionalMessage){
-    // this tests _that_ the expected exception is thrown
     EXPECT_THROW({
-                     try
-                     {
+                     try {
                          throw PIL::Exception(PIL_INTERFACE_CLOSED, __FILENAME__, __LINE__, "Additional Message %d", 42);
                      }
-                     catch(PIL::Exception &e)
-                     {
-                         // and this tests that it has the correct message
+                     catch(PIL::Exception &e) {
                          EXPECT_STREQ( "Exception (ExceptionUnitTest.cpp:43): Socket is closed (Additional Message 42)", e.what() );
                          throw;
                      }
@@ -52,15 +41,11 @@ TEST(ExceptionTest, ExceptionFileNameLineNumberAdditionalMessage){
 }
 
 TEST(ExceptionTest, ExceptionFileNameLineNumberNoAdditionalMessage){
-    // this tests _that_ the expected exception is thrown
     EXPECT_THROW({
-                     try
-                     {
+                     try {
                          throw PIL::Exception(PIL_INTERFACE_CLOSED, __FILENAME__, __LINE__);
                      }
-                     catch(PIL::Exception &e)
-                     {
-                         // and this tests that it has the correct message
+                     catch(PIL::Exception &e) {
                          EXPECT_STREQ( "Exception (ExceptionUnitTest.cpp:59): Socket is closed", e.what() );
                          throw;
                      }
