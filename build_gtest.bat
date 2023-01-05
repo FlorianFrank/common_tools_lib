5aa06e9b81eab49d0fbcbde92d128c1c7c8bfded
Florian Frank <FlorianFrank@users.noreply.github.com>
2023-01-05 09:49:36 +0100

    Feature/add gtest as submodule (#27)
    * Add gtest as submodule to the repo
    
    * Remove not supported apt-get command on macos github action
    
    * Cleanup and improve github workflows

--- build_gtest.bat
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/google/googletest
-cd googletest
-mkdir build
-cd build
-cmake .. -GNinja -DCMAKE_CXX_COMPILER=C:/ProgramData/chocolatey/bin/g++.exe -DCMAKE_C_COMPILER=C:/ProgramData/chocolatey/bin/gcc.exe
-ninja
-cd ../..
