108c3d9b5c1cf844dc28b1b7ad78662d31abf3a6
Florian Frank <Florian.Frank@uni-passau.de>
2023-01-15 20:22:06 +0100

    Add new constructor and setters to the socket implementation, additionally fix error when passing the responsibility of raw pointers to smart ptrs.

--- Communication/include/ctlib/Socket.hpp
+++ Communication/include/ctlib/Socket.hpp
@@ -21,9 +21,29 @@ namespace PIL
     class Socket
     {
     public:
+
+        struct ReceiveCallbackArg {
+            explicit ReceiveCallbackArg(std::function<void(std::shared_ptr<PIL::Socket>&, std::string&)>& c): m_ReceiveCallback(c){
+            }
+            std::function<void(std::shared_ptr<PIL::Socket>&, std::string&)> &m_ReceiveCallback;
+            std::shared_ptr<PIL::Socket> m_Socket = {};
+        };
+
+        /**
+         * @brief Workaround to pass std::functions to C-acceptCallback function.
+         */
+        struct ThreadAcceptArg
+        {
+            /** Old C-threading function. */
+            AcceptThreadArgC argC = {};
+            /** Function pointer to C++ function returning PIL::Socket object. */
+            std::function<void(std::unique_ptr<PIL::Socket> &)> acceptCallback = {};
+        };
+
+        Socket();
         Socket(TransportProtocol transportProtocol, InternetProtocol internetProtocol, const std::string &address,
                int port, uint16_t timeoutInMS);
-        Socket(std::unique_ptr<PIL_SOCKET> socket, std::string &ip, uint16_t port);
+        Socket(std::shared_ptr<PIL_SOCKET> &socket, std::string &ip, uint16_t port);
         ~Socket();
 
         PIL_ERROR_CODE Bind(PIL_BOOL reuse);
@@ -44,35 +64,20 @@ namespace PIL
         PIL_ERROR_CODE Send(std::string &message);
         PIL_ERROR_CODE SendTo(std::string &destAddr, int port, uint8_t *buffer, int *bufferLen);
 
-        std::string GetSenderIP();
+        PIL_ERROR_CODE CreateServerSocket(std::function<void(std::unique_ptr<PIL::Socket>&)> &receiveCallback);
+        PIL_ERROR_CODE ConnectToServer(std::string &ipAddr, int destPort, std::function<void(std::shared_ptr<Socket>& , std::string &)> &receiveCallback);
+
+        PIL_ERROR_CODE RegisterReceiveCallbackFunction(ReceiveCallbackArg& additionalArg);
+        PIL_ERROR_CODE UnregisterAllCallbackFunctions();
+
+        std::string GetSenderIPAddress();
         PIL_ERROR_CODE GetInterfaceInfos(InterfaceInfoList *interfaceInfos);
         PIL_BOOL IsOpen();
         TransportProtocol GetTransportProtocol() const { return m_TransportProtocol; }
         InternetProtocol GetInternetProtocol() const { return m_InternetProtocol; }
-
-        PIL_ERROR_CODE CreateServerSocket(std::function<void(std::unique_ptr<PIL::Socket>&)> &receiveCallback);
-        PIL_ERROR_CODE ConnectToServer(std::string &ipAddr, int destPort, std::function<void(std::unique_ptr<Socket>& , std::string &)> &receiveCallback);
-
-
-        struct ReceiveCallbackArg {
-            explicit ReceiveCallbackArg(std::function<void(std::unique_ptr<PIL::Socket>&, std::string&)>& c): m_ReceiveCallback(c){
-            }
-            std::function<void(std::unique_ptr<PIL::Socket>&, std::string&)> &m_ReceiveCallback;
-            std::unique_ptr<PIL::Socket> m_Socket = {};
-        };
-
-        PIL_ERROR_CODE RegisterReceiveCallbackFunction(ReceiveCallbackArg& additionalArg);
-        PIL_ERROR_CODE UnregisterCallbackFunction();
-
-        /**
- * @brief Workaround to pass std::functions to C-acceptCallback function.
- */
-        struct ThreadAcceptArg {
-            /** Old C-threading function. */
-            AcceptThreadArgC argC = {};
-            /** Function pointer to C++ function returning PIL::Socket object. */
-            std::function<void(std::unique_ptr<PIL::Socket>&)> acceptCallback = {};
-        };
+        void setPort(uint16_t mPort);
+        void setIPAddress(const std::string &mIpAddress);
+        void setSocketHandle(const std::shared_ptr<PIL_SOCKET> &mCSocketHandle);
 
     private:
         uint16_t m_Port;
@@ -81,10 +86,12 @@ namespace PIL
         InternetProtocol m_InternetProtocol;
         uint16_t m_TimeoutInMS;
 
-        std::unique_ptr<PIL_SOCKET> m_CSocketHandle;
+
+        std::shared_ptr<PIL_SOCKET> m_CSocketHandle;
         std::vector<std::unique_ptr<PIL_SOCKET>> m_SocketList;
        // std::unique_ptr<ThreadAcceptArg> m_ThreadArg;
         std::unique_ptr<PIL::Threading<ThreadAcceptArg>> m_AcceptThread;
+        std::unique_ptr<ReceiveCallbackArg> m_ReceiveCallback;
         PIL_ERROR_CODE RegisterAcceptCallback(std::function<void(std::unique_ptr<PIL::Socket>&)> &f);
     };
 
