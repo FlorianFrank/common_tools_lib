18e0775ab53b345f7f1fe1184397ec1cad550c38
Florian Frank <Florian.Frank@uni-passau.de>
2023-01-22 20:09:35 +0100

    Fix error of incorrect side bar in NSIS installer

--- cmake/installers/windows_nsis_installer.cmake
+++ cmake/installers/windows_nsis_installer.cmake
@@ -34,9 +34,15 @@ function(build_nsis_installer)
     set(CPACK_SOURCE_GENERATOR "TGZ")
     set(CPACK_SOURCE_PACKAGE_FILE_NAME "common_tools_lib_1.0.0")
 
-    set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${CMAKE_CURRENT_LIST_DIR}/cmake/installers/nsis_bundle/nsis_welcome_finish_page.bmp")
+    set (CPACK_VERBATIM_VARIABLES ON)
+
     set(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_LIST_DIR}/cmake/installers/nsis_bundle/logo.ico")
     set(CPACK_NSIS_MUI_UNIICON "${CMAKE_CURRENT_LIST_DIR}/cmake/installers/nsis_bundle/logo.ico")
+    set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP  "${CMAKE_CURRENT_LIST_DIR}/cmake/installers/nsis_bundle/sidebar.bmp")
+    set(CPACK_NSIS_MUI_HEADERIMAGE "${CMAKE_CURRENT_LIST_DIR}/cmake/installers/nsis_bundle/header.bmp")
+    string(REGEX REPLACE "/" "\\\\" CPACK_NSIS_MUI_ICON "${CPACK_NSIS_MUI_ICON}")
+    string(REGEX REPLACE "/" "\\\\" CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP}")
+    string(REGEX REPLACE "/" "\\\\" CPACK_NSIS_MUI_HEADERIMAGE "${CPACK_NSIS_MUI_HEADERIMAGE}")
 
     set(CPACK_NSIS_URL_INFO_ABOUT "https://florianfrank.github.io/")
     set(CPACK_NSIS_CONTACT "Florian.Frank@uni-passau.de")
